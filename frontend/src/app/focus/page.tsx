07cee8e5bbdb7651b8d636e8b8c8331f6f31f19a
Keshav Agrawal <keshavagrawal21@gmail.com>
2026-03-31 14:01:47 +0530

    focus page static UI and with progress handlers

--- frontend/src/app/focus/page.tsx
+++ frontend/src/app/focus/page.tsx
@@ -1,5 +1,5 @@
 import { FocusComponent } from '@/components/Focus/focus'
 
 export default function FocusPage() {
-  return <FocusComponent />
+  return <FocusComponent totalSessions={3} time={0.1} pauseTime={0.1} />
 }
