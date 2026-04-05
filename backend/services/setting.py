871acb4e32d8515eaaa4ae28cdd88c05a3a384cd
ananyarao23 <ananya.rao.2005@gmail.com>
2026-04-05 16:33:01 +0530

    reformat BE2

--- backend/services/setting.py
+++ backend/services/setting.py
@@ -1,19 +1,15 @@
 from db.supabase import supabase
 
+
 async def update_settings_data(type: str, data: dict, user_id: int):
     if type == "profile":
-        response = (
-            supabase.table("users")
-            .update(data)
-            .eq("id", user_id)
-            .execute()
-        )
-        
+        response = supabase.table("users").update(data).eq("id", user_id).execute()
+
         return response.data
 
     elif type == "focus":
-        #Update in db
-        
+        # Update in db
+
         response = None
         return response.data
 
