5edecfd2e80e78d2c7f8af6b9a36d4c33916dbbf
ananyarao23 <ananya.rao.2005@gmail.com>
2026-04-07 13:13:09 +0530

    added frontend page too, will conflict with existing

--- frontend/src/app/tasks/page.tsx
+++ frontend/src/app/tasks/page.tsx
@@ -1,5 +1,49 @@
-import { TasksPageComponent } from '@/components/Tasks/tasks'
+'use client'
+
+import { AddTaskFormComponent } from '@/components/Tasks/tasks'
+import { TaskListComponent } from '@/components/Tasks/tasks'
+import { useState, useEffect } from 'react'
+import { apiClient } from '@/services/api'
+
+
+interface Task {
+  id: number
+  title: string
+  description: string
+  deadline: string
+  priority: string
+}
 
 export default function TaskPage() {
-  return <TasksPageComponent />
+
+  const [tasks, setTasks] = useState<Task[]>([])
+  const [loading, setLoading] = useState<boolean>(true)
+
+  const fetchTasks = async() => {
+    try {
+      const response = await apiClient.get('/tasks')
+      setTasks(response as Task[])
+    } catch (error) {
+      console.error('Error fetching tasks:', error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchTasks()
+  }, [])
+
+  return (
+    <div className="p-4 flex v-screen">
+      <div className="w-[65%] pr-4">
+        <h1 className="text-2xl font-bold mb-4 text-black">My Tasks</h1>
+        <TaskListComponent tasks={tasks} />
+      </div>
+      <div className='w-[35%] pl-4 border-l'>
+        <h1 className="text-2xl font-bold mb-4 text-black">Tasks</h1>
+        <AddTaskFormComponent onTaskAdded={() => {}} />
+      </div>
+    </div>
+  )
 }
