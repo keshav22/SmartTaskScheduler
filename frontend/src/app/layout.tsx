ee86ddee311b6d7a608c8201b4f2a9758ea2c1a0
Keshav Agrawal <keshavagrawal21@gmail.com>
2026-04-07 16:55:10 +0530

    Tasks screen UI with modal popup for add/edit tasks

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import { LeftPanel } from '@/components/Navbar/left_panel'
-import { Inter } from 'next/font/google'
+import { Inter, Geist } from 'next/font/google'
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -19,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", geist.variable)}>
       <body className={`${inter.className} antialiased bg-[#f9f9f9]`}>
         <LeftPanel />
         <div className="pl-66">{children}</div>
--- frontend/src/components/Focus/circular_progress.tsx
+++ frontend/src/components/Focus/circular_progress.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { cn } from '@/utils/css'
+import { cn } from '@/lib/utils'
 import { ReactNode } from 'react'
 
 interface CircularProgressProps {
