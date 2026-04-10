2bcaeadb8bd968bb2976a412a789e2d59d5c0ec4
ananyarao23 <ananya.rao.2005@gmail.com>
2026-04-10 20:15:31 +0530

    lint

--- backend/services/user.py
+++ backend/services/user.py
@@ -2,11 +2,6 @@ from db.supabase import supabase
 
 
 async def get_user_focus_settings(user_id):
-    return (
-        supabase.table("users")
-        .select("""
+    return supabase.table("users").select("""
         session_duration, break_duration
-    """)
-        .eq("id", user_id)
-        .execute()
-    )
+    """).eq("id", user_id).execute()
