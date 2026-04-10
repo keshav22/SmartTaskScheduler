ec97907f9f12aba2cd843fe02ba5284a2c778310
ananyarao23 <ananya.rao.2005@gmail.com>
2026-04-10 20:10:30 +0530

    lint

--- backend/routers/setting.py
+++ backend/routers/setting.py
@@ -8,10 +8,12 @@ router = APIRouter()
 @router.patch("/settings")
 async def update_settings(request: Request, task_data: dict):
     try:
-        
+
         user_id = get_current_user_from_state(request)["sub"]
         print("User ID:", user_id)
-        result = await update_settings_data(user_id, task_data["data"])  # user_id required too
+        result = await update_settings_data(
+            user_id, task_data["data"]
+        )  # user_id required too
         print("Updated succe3ssfully:", result)
         return {"success": True, "data": result}
     except Exception as e:
--- backend/services/setting.py
+++ backend/services/setting.py
@@ -3,11 +3,6 @@ from db.supabase import supabase
 
 async def update_settings_data(user_id, task_data: dict):
     print(task_data)
-    res = (
-        supabase.table("users")
-        .update(task_data)
-        .eq("id", user_id)
-        .execute()
-    )
+    res = supabase.table("users").update(task_data).eq("id", user_id).execute()
 
     return res
